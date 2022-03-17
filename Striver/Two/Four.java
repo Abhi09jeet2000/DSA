c88f2df262acd2a2429c27c2c3b77c84b9a91fb1
Abhi09jeet2000 <abhirbkulkarni@gmail.com>
2022-03-17 14:06:51 +0530

    Find the Duplicate Number

--- Striver/Two/Four.java
+++ Striver/Two/Four.java
@@ -1,12 +1,25 @@
 class Four{
     public int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums){
-            if(set.contains(i)){
-                return i;
-            }
-            set.add(i);
+        // Set<Integer> set = new HashSet<>();
+        // for(int i : nums){
+        //     if(set.contains(i)){
+        //         return i;
+        //     }
+        //     set.add(i);
+        // }
+        // return 0;
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        fast = nums[0];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return fast;
     }
 }
